a6dd2cedb83ca1948fa1ada8ed5d0438efe1012d
Santiago Rendon <santirg2000@gmail.com>
2021-10-24 03:13:34 -0500

    Manage promises with asnyc/await instead of SELENIUM_PROMISE_MANAGER

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -1,11 +1,13 @@
 import { browser, Config } from 'protractor';
+import { reporter } from './helpers/reporter';
 
 export const config: Config = {
   framework: 'jasmine',
+  SELENIUM_PROMISE_MANAGER: false,
   specs: ['../test/google.spec.js'],
-  seleniumAddress: 'http://localhost:4444/wd/hub',
   onPrepare: () => {
     // Shows deprecated but will keep for workshop
     browser.ignoreSynchronization = true;
+    reporter();
   },
 };
