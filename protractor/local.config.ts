0c049b6c08397e3aefb35cb2192e4fcf0eada3db
Santiago Rendón <52801967+Rendxn@users.noreply.github.com>
2021-10-24 02:36:45 -0500

    Improving protactor tests (#1)

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -1,7 +1,11 @@
-import { Config } from "protractor";
+import { browser, Config } from 'protractor';
 
 export const config: Config = {
-  framework: "jasmine",
-  specs: ["../test/google.spec.js"],
-  seleniumAddress: "http://localhost:4444/wd/hub",
+  framework: 'jasmine',
+  specs: ['../test/google.spec.js'],
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  onPrepare: () => {
+    // Shows deprecated but will keep for workshop
+    browser.ignoreSynchronization = true;
+  },
 };
