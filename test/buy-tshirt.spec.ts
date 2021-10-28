723396d357ff5a61d4772d34004c920c67200b66
Santiago Rendón <52801967+Rendxn@users.noreply.github.com>
2021-10-27 19:19:43 -0500

    Multiple nested describes for better test description (#4)

--- test/buy-tshirt.spec.ts
+++ test/buy-tshirt.spec.ts
@@ -20,34 +20,63 @@ import {
 describe('Buy a t-shirt', () => {
   const EC: ProtractorExpectedConditions = ExpectedConditions;
 
-  const menuContentPage: MenuContentPage = new MenuContentPage();
-  const productListPage: ProductListPage = new ProductListPage();
-  const productAddedModalPage: ProductAddedModalPage = new ProductAddedModalPage();
-  const summaryStepPage: SummaryStepPage = new SummaryStepPage();
-  const signInStepPage: SignInStepPage = new SignInStepPage();
-  const addressStepPage: AddressStepPage = new AddressStepPage();
-  const shippingStepPage: ShippingStepPage = new ShippingStepPage();
-  const paymentStepPage: PaymentStepPage = new PaymentStepPage();
-  const bankPaymentStepPage: BankPaymentPage = new BankPaymentPage();
-  const orderSummaryPage: OrderSummaryPage = new OrderSummaryPage();
+  describe('Given a page', async () => {
+    beforeAll(async () => {
+      await browser.get('http://automationpractice.com/');
+    });
 
-  it('then should be bought a t-shirt', async () => {
-    await browser.get('http://automationpractice.com/');
-    await menuContentPage.goToTShirtMenu();
-    await browser.wait(EC.elementToBeClickable(productListPage.AddToCartBtn), 3000);
-    await productListPage.addToCart();
-    await browser.wait(EC.elementToBeClickable(productAddedModalPage.ProceedToCheckoutBtn), 3000);
-    await productAddedModalPage.goToCheckout();
-    await summaryStepPage.goToCheckout();
-    await signInStepPage.fillForm();
-    await signInStepPage.signIn();
-    await addressStepPage.goToCheckout();
-    await shippingStepPage.acceptTerms();
-    await shippingStepPage.goToCheckout();
-    await paymentStepPage.choosePaymentMethod();
-    await bankPaymentStepPage.confirmOrder();
+    describe('when wanting to buy a  t-shirt', async () => {
+      beforeAll(async () => {
+        const menuContentPage: MenuContentPage = new MenuContentPage();
+        const productListPage: ProductListPage = new ProductListPage();
+        const productAddedModalPage: ProductAddedModalPage = new ProductAddedModalPage();
+        const summaryStepPage: SummaryStepPage = new SummaryStepPage();
 
-    await expect(orderSummaryPage.confirmationMessage())
-      .toBe('Your order on My Store is complete.');
+        await menuContentPage.goToTShirtMenu();
+        await browser.wait(EC.elementToBeClickable(productListPage.AddToCartBtn), 3000);
+        await productListPage.addToCart();
+        await browser.wait(EC.elementToBeClickable(productAddedModalPage.ProceedToCheckoutBtn),
+          3000);
+        await productAddedModalPage.goToCheckout();
+        await summaryStepPage.goToCheckout();
+      });
+
+      describe('Sign in the webpage', async () => {
+        beforeAll(async () => {
+          const signInStepPage: SignInStepPage = new SignInStepPage();
+
+          await signInStepPage.fillForm();
+          await signInStepPage.signIn();
+        });
+
+        describe('Choose default address', async () => {
+          beforeAll(async () => {
+            const addressStepPage: AddressStepPage = new AddressStepPage();
+            const shippingStepPage: ShippingStepPage = new ShippingStepPage();
+
+            await addressStepPage.goToCheckout();
+            await shippingStepPage.acceptTerms();
+            await shippingStepPage.goToCheckout();
+          });
+
+          describe('Pay order', async () => {
+            beforeAll(async () => {
+              const paymentStepPage: PaymentStepPage = new PaymentStepPage();
+              const bankPaymentStepPage: BankPaymentPage = new BankPaymentPage();
+
+              await paymentStepPage.choosePaymentMethod();
+              await bankPaymentStepPage.confirmOrder();
+            });
+
+            it('then should be bought a t-shirt', async () => {
+              const orderSummaryPage: OrderSummaryPage = new OrderSummaryPage();
+
+              await expect(orderSummaryPage.confirmationMessage())
+                .toBe('Your order on My Store is complete.');
+            });
+          });
+        });
+      });
+    });
   });
 });
